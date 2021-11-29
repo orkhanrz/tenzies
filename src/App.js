9ef9245f6e7c8c516d56781baf75d19c423d07ef
Orkhan Rzali <orkhan.rz8@gmail.com>
2021-11-29 15:31:25 +0400

    styling added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tenzies">
+      <h1 className="header">Tenzies</h1>
+      <p className="paragraph">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice"></div>
+      <button className="button">Roll</button>
     </div>
   );
 }
-
-export default App;
